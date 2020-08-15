c0f6cf55584eb776c974b8c192df1ecbfde03595
Danial Jalaluddin <dan90@live.com.my>
2020-08-15 23:01:43 +0800

    updated

--- flashcards.py
+++ flashcards.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template,abort,jsonify
+from flask import (Flask, render_template,abort,jsonify,request,
+                   redirect,url_for)
 # from datetime import datetime
-from model import db
+from model import db, save_db
 
 
 app = Flask(__name__)
@@ -40,10 +41,31 @@ def card_view(index):
     except IndexError:
         abort(404)
 
-@app.route('/add_card')
+@app.route('/add_card',methods=['GET','POST'])
 def add_card():
-    return render_template('add_card.html')
-    
+    if request.method == 'POST':
+        card = {"question":request.form['question'],
+                "answer":request.form['answer']}
+        db.append(card)
+        save_db()
+        return redirect(url_for('card_view',index=len(db)-1))
+    else:
+        return render_template("add_card.html")
+
+@app.route('/remove_card/<int:index>',methods=["GET","POST"])
+def remove_card(index):
+    try:
+        if request.method == "POST":
+            del db[index]
+            save_db()
+            return redirect(url_for('welcome'))
+        else:
+            return render_template("remove_card.html",card=db[index])
+    except IndexError:
+        abort(404)   
+        
+
+
 
 # @app.route("/date")    
 # def date():
--- model.py
+++ model.py
@@ -4,4 +4,9 @@ def load_db():
     with open("flashcards_db.json",errors='ignore') as f:
         return json.load(f)
     
+def save_db():
+    with open("flashcards_db.json","w") as f:
+        return json.dump(db,f)    
+    
 db = load_db()    
+
